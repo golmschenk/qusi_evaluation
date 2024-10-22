b9b2dd5bd5aa04979f38a0e8c7624b0a2aa9c167
golmschenk <greg@olmschenk.com>
2024-10-22 11:20:28 -0400

    Add SLB example

--- src/qusi_evaluation/download_spoc_sector_27_to_56_light_curve_data.py
+++ src/qusi_evaluation/download_spoc_sector_27_to_56_light_curve_data.py
@@ -5,7 +5,7 @@ from qusi.experimental.application.tess import (
     get_spoc_tic_id_list_from_mast,
 )
 
-spoc_sector_27_to_56_light_curve_directory = Path('data/spoc_sector_27_to_56_light_curves')
+spoc_sector_27_to_55_light_curve_directory = Path('data/spoc_sector_27_to_55_light_curves')
 
 
 def main():
@@ -16,7 +16,7 @@ def main():
     print('Downloading light curves...')
     download_spoc_light_curves_for_tic_ids(
         tic_ids=spoc_target_tic_ids,
-        download_directory=spoc_sector_27_to_56_light_curve_directory,
+        download_directory=spoc_sector_27_to_55_light_curve_directory,
         sectors=sectors)
 
 
--- src/qusi_evaluation/slb_dataset.py
+++ src/qusi_evaluation/slb_dataset.py
@@ -14,12 +14,18 @@ def get_spoc_10_minute_ffi_light_curve_paths():
     return paths
 
 
-def get_rich_barry_synthetic_microlensing_paths():
+def get_synthetic_slb_paths():
     paths: list[Path] = []
-    for path in Path('data/microlensing_training_lightcurves').glob('**/*.txt'):
-        if re.match(r'Lc_\d+\.txt', path.name):
+    for path in Path('data/slb_synthetic_signals').glob('**/*.dat'):
+        if re.match(r'SLB_pos\.\d+\.dat', path.name):
             paths.append(path)
-    print(f'RBMLC: {len(paths)}, flush=True')
+    return paths
+
+def get_synthetic_non_slb_paths():
+    paths: list[Path] = []
+    for path in Path('data/slb_synthetic_signals').glob('**/*.dat'):
+        if re.match(r'SLB_neg\.\d+\.dat', path.name):
+            paths.append(path)
     return paths
 
 
@@ -28,8 +34,8 @@ def load_times_and_fluxes_from_tess_path(path):
     return light_curve.times, light_curve.fluxes
 
 
-def load_times_and_magnifications_from_rich_barry_path(path: Path) -> (npt.NDArray, npt.NDArray):
-    light_curve = np.loadtxt(path, dtype=np.float32)
+def load_times_and_magnifications_from_synthetic_path(path: Path) -> (npt.NDArray, npt.NDArray):
+    light_curve = np.loadtxt(path, dtype=np.float32, skiprows=1)
     times = light_curve[:, 0]
     fluxes = light_curve[:, 1]
     return times, fluxes
@@ -43,18 +49,23 @@ def negative_label_function(path):
     return 0
 
 
-def get_microlensing_train_dataset():
+def get_slb_train_dataset():
     tess_light_curve_collection = LightCurveObservationCollection.new(
         get_paths_function=get_spoc_10_minute_ffi_light_curve_paths,
         load_times_and_fluxes_from_path_function=load_times_and_fluxes_from_tess_path,
         load_label_from_path_function=negative_label_function)
     synthetic_microlensing_light_curve_collection = LightCurveObservationCollection.new(
-        get_paths_function=get_rich_barry_synthetic_microlensing_paths,
-        load_times_and_fluxes_from_path_function=load_times_and_magnifications_from_rich_barry_path,
+        get_paths_function=get_synthetic_slb_paths,
+        load_times_and_fluxes_from_path_function=load_times_and_magnifications_from_synthetic_path,
         load_label_from_path_function=positive_label_function)
+    non_synthetic_microlensing_light_curve_collection = LightCurveObservationCollection.new(
+        get_paths_function=get_synthetic_non_slb_paths,
+        load_times_and_fluxes_from_path_function=load_times_and_magnifications_from_synthetic_path,
+        load_label_from_path_function=negative_label_function)
     train_light_curve_dataset = LightCurveDataset.new(
         standard_light_curve_collections=[tess_light_curve_collection],
         injectee_light_curve_collections=[tess_light_curve_collection],
-        injectable_light_curve_collections=[synthetic_microlensing_light_curve_collection],
+        injectable_light_curve_collections=[synthetic_microlensing_light_curve_collection,
+                                            non_synthetic_microlensing_light_curve_collection],
     )
     return train_light_curve_dataset
